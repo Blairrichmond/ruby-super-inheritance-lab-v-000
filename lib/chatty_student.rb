79eba743d5d415006329c3a3a06d8ac9dd09477a
Brent  Ransom <sadumedusa@gmail.com>
2018-02-21 19:06:49 +0000

    automatically backed up by learn

--- lib/chatty_student.rb
+++ lib/chatty_student.rb
@@ -2,7 +2,7 @@ class ChattyStudent < Student
 
   def hello
     super
-    puts "Hey there! I'm so excited to learn stuff. How are you doing today? I'm okay, but I'm kind of tired. Did you watch The Walking Dead last 
+    puts "Hey there! I'm so excited to learn stuff. How are you doing today? I'm okay, but I'm kind of tired. Did you watch The Walking Dead last
     night? You didn't?! Oh man, it was so crazy! What, you don't want any spoilers? Okay well let me just tell you who died..."
   end
 
@@ -13,6 +13,6 @@ class ChattyStudent < Student
     while 0 < 10
     puts "Pick me!"
   end
-    
+
   end
 end
